e94c85ca4cc6fc509b10ec51f1cf336c1d785fce
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-11-09 01:20:46 +0600

    mongodb database and secrete key setUp

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleWare 
 app.use(cors())
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ylc1dr9.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 app.get('/', (req, res) => {
     res.send('MidTro Server Side is Running')
