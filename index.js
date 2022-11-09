8cd29873052771069dcc4145e209ae4bd6298dc5
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-11-09 20:42:28 +0600

    added delete read property

--- index.js
+++ index.js
@@ -18,14 +18,14 @@ async function run() {
         const serviceCollection = client.db('mediTro').collection('services')
         const orderCollection = client.db('mediTro').collection('orders')
 
-        app.get('/services', async(req, res) => {
+        app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services);
         })
 
-        app.get('/services/:id', async(req, res) => {
+        app.get('/services/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const service = await serviceCollection.findOne(query);
@@ -33,17 +33,30 @@ async function run() {
         })
 
         // orders api 
-        app.get('/orders', async(req, res) => {
-            console.log(req.query);
-            const query = {};
+        app.get('/orders', async (req, res) => {
+            let query = {};
+
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+
             const cursor = orderCollection.find(query);
             const orders = await cursor.toArray();
             res.send(orders);
         })
 
-        app.post('/orders', async(req, res) => {
+        app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order);
+            res.send(result);
+        })
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await orderCollection.deleteOne(query)
             res.send(result);
         })
 
